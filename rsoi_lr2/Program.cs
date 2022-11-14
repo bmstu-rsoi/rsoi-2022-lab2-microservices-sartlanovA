a7472a10efbce8f26e9e026593b8dd09efe33f40
sartlanovA <alina_sartlanova@mail.ru>
2022-11-14 19:03:11 +0300

    rsoi_lr2

---
+++ rsoi_lr2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rsoi_lr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
